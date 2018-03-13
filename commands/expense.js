68200536ad43dfe38106b6e2d94875a73c54c1e5
Muhammad Irfan <m.irfan@bukalapak.com>
2018-03-13 21:19:08 +0700

    add scenario choose account

--- commands/expense.js
+++ commands/expense.js
@@ -1,3 +1,6 @@
+const sqlite3 = require('sqlite3').verbose()
+const db = new sqlite3.Database('database')
+
 module.exports = {
   validate: function(text) {
     const textArr = text.split(' ')
@@ -12,18 +15,38 @@ module.exports = {
   },
 
   process: function(msg, bot) {
+    // Setup variables
+    const textArr = msg.text.split('|')
     var option = {
       "parse_mode": "Markdown",
       "reply_markup": JSON.stringify({
           "one_time_keyboard": true,
-          "keyboard": [
-            [{ text: `${msg.text} - BCA` }],
-            [{ text: `${msg.text} - CommBank` }],
-            [{ text: `${msg.text} - Mandiri` }],
-          ]
+          "keyboard": []
       })
     }
 
-    return bot.sendMessage(msg.chat.id, "processing expense...", option)
+    // Scenario 1: Haven't choose fund source
+    if (!textArr.length) {
+      let keyboard = []
+
+      db.serialize(function() {
+        db.each("SELECT * FROM account", function(err, row) {
+          keyboard.push(row.name)
+        });
+      })
+
+      option.reply_markup.keyboard = keyboard
+      return bot.sendMessage(msg.chat.id, "Tolong pilih sumber dana", option)
+    }
+    // Scenario 2: Chose fund source but haven't choose category
+    else if (textArr.length === 2) {
+
+    }
+    // Scenario 3: Command complete
+    else {
+
+    }
+
+    return bot.sendMessage(msg.chat.id, "Data berhasil disimpan")
   }
 }
