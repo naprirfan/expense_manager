66c984588c77a749fe76be2c283801e1326ce90d
Muhammad Irfan <m.irfan@bukalapak.com>
2018-03-14 20:00:36 +0700

    add scenario 2

--- commands/expense.js
+++ commands/expense.js
@@ -68,6 +68,46 @@ module.exports = {
     }
     // Scenario 2: Chose fund source but haven't choose category
     else if (textArr.length === 2) {
+      db.serialize(function() {
+        db.all("SELECT * FROM expense_category", function(err, all) {
+          let keyboard = []
+
+          let entry = []
+          all.forEach(item => {
+            if (entry.length === 3) {
+              keyboard.push(entry)
+              entry = [{ text: item.name }]
+            }
+            else {
+              entry.push({ text: item.name })
+            }
+          })
+
+          // Add new account
+          const newCategory = { text: 'Tambah kategori baru >>'}
+          if (entry.length === 3) {
+            keyboard.push(entry)
+            keyboard.push([newCategory])
+          }
+          else {
+            entry.push(newCategory)
+            keyboard.push(entry)
+          }
+
+          var option = {
+            "parse_mode": "Markdown",
+            "reply_markup": JSON.stringify({
+                "one_time_keyboard": true,
+                "keyboard": keyboard
+            })
+          }
+
+          db.run(`UPDATE context SET key = 'expense', value = '${input}' WHERE chat_id = ${chat_id}`, (err, row) => {
+            if (err) return bot.sendMessage(chat_id, 'Error.. Silakan coba lagi')
+            return bot.sendMessage(chat_id, 'Pilih Kategori Belanja Anda', option)
+          })
+        })
+      })
       return bot.sendMessage(chat_id, "Ini inputnya: " + input)
     }
     // Scenario 3: Command complete
