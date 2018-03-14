e8b24d2f29e1f4af6e9447b5b22c0992a4e34c4c
Muhammad Irfan <m.irfan@bukalapak.com>
2018-03-14 21:39:15 +0700

    refactor

--- commands/expense.js
+++ commands/expense.js
@@ -41,37 +41,8 @@ module.exports = {
     else if (textArr.length === 2) {
       db.serialize(function() {
         db.all("SELECT * FROM expense_category", function(err, all) {
-          let keyboard = []
 
-          let entry = []
-          all.forEach(item => {
-            if (entry.length === 3) {
-              keyboard.push(entry)
-              entry = [{ text: item.name }]
-            }
-            else {
-              entry.push({ text: item.name })
-            }
-          })
-
-          // Add new account
-          const newCategory = { text: 'Tambah kategori baru >>'}
-          if (entry.length === 3) {
-            keyboard.push(entry)
-            keyboard.push([newCategory])
-          }
-          else {
-            entry.push(newCategory)
-            keyboard.push(entry)
-          }
-
-          var option = {
-            "parse_mode": "Markdown",
-            "reply_markup": JSON.stringify({
-                "one_time_keyboard": true,
-                "keyboard": keyboard
-            })
-          }
+          let option = helper.enrichKeyboard(collection, 'Tambah kategori baru >>')
 
           db.run(`UPDATE context SET key = 'expense', value = '${input}' WHERE chat_id = ${chat_id}`, (err, row) => {
             if (err) return bot.sendMessage(chat_id, 'Error.. Silakan coba lagi')
