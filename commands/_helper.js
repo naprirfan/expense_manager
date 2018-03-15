d784219294fae9f7a6f37fe0709e0dea7ec997d9
Muhammad Irfan <m.irfan@bukalapak.com>
2018-03-15 13:00:34 +0700

    rename database query string

--- commands/_helper.js
+++ commands/_helper.js
@@ -1,5 +1,5 @@
 const sqlite3 = require('sqlite3').verbose()
-const db = new sqlite3.Database('database')
+const db = new sqlite3.Database('../database/database')
 
 module.exports = {
   enrichKeyboard: function(collection, additonal_text) {
--- commands/expense.js
+++ commands/expense.js
@@ -1,5 +1,5 @@
 const sqlite3 = require('sqlite3').verbose()
-const db = new sqlite3.Database('database')
+const db = new sqlite3.Database('../database/database')
 const helper = require('./_helper.js')
 
 module.exports = {
