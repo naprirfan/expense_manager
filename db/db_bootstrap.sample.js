78945da52e6389e9592d5a76572724aa84bed952
Muhammad Irfan <naprirfan@gmail.com>
2018-03-15 06:24:28 +0000

    modify path

--- db/db_bootstrap.sample.js
+++ db/db_bootstrap.sample.js
@@ -1,5 +1,5 @@
 var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('db/database');
+var db = new sqlite3.Database('database');
 var config = require('../config.js')
 
 db.serialize(function() {
