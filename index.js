95d72f38d34a28efba01687e2285b41719b35e09
Muhammad Irfan <naprirfan@gmail.com>
2018-03-23 03:14:50 +0000

    created pdf

--- index.js
+++ index.js
@@ -36,9 +36,10 @@ app.use(express.static('public'))
 app.get(`/expense_manager/generate_report`, (req, res) => {
   ejs.renderFile('./views/report_template.ejs.html', {}, {}, function(err, str){
     const options = { format: 'Letter' };
-    pdf.create(html, options).toFile(`./reports/report${new Date()}.pdf`, function(err, res) {
+    pdf.create(str, options).toFile(`./public/reports/report${new Date()}.pdf`, function(err, compiled) {
       if (err) return console.log(err);
-      return res.end('ok')
+      console.log(compiled)
+      return res.download(compiled.filename)
     });
   });
 })
