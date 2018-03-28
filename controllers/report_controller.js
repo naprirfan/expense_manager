bfd9f58dd12af7d64e58ac729e77dfc622d7ec0c
Muhammad Irfan <m.irfan@bukalapak.com>
2018-03-28 21:25:04 +0700

    refactor

--- controllers/report_controller.js
+++ controllers/report_controller.js
@@ -79,31 +79,31 @@ const reportCtrl = {
           account: accountRows,
         }
 
-        this.createPDF(res, data)
+        ejs.renderFile('views/report_template.ejs.html', data, {}, function(err, str){
+          const options = { format: 'Letter' }
+          const now = Math.floor(new Date() / 1000)
+
+          pdf.create(str, options).toFile(`../reports/report${now}.pdf`, function(err, compiled) {
+            if (err) return console.log(err)
+
+            const cmd = `qpdf --encrypt ${config.PDF_PASSWORD} ${config.PDF_PASSWORD} 40 -- ${compiled.filename} ../reports/encrypted_report_${now}.pdf`
+            exec(cmd, function (err) {
+              if (err) {
+                return res.end('Error occured: ' + err)
+              }
+              else {
+                return res.download(`../reports/encrypted_report_${now}.pdf`)
+              }
+            })
+          })
+        })
       })
     })
 
   },
 
   createPDF: (res, data) => {
-    ejs.renderFile('views/report_template.ejs.html', data, {}, function(err, str){
-      const options = { format: 'Letter' }
-      const now = Math.floor(new Date() / 1000)
 
-      pdf.create(str, options).toFile(`../reports/report${now}.pdf`, function(err, compiled) {
-        if (err) return console.log(err)
-
-        const cmd = `qpdf --encrypt ${config.PDF_PASSWORD} ${config.PDF_PASSWORD} 40 -- ${compiled.filename} ../reports/encrypted_report_${now}.pdf`
-        exec(cmd, function (err) {
-          if (err) {
-            return res.end('Error occured: ' + err)
-          }
-          else {
-            return res.download(`../reports/encrypted_report_${now}.pdf`)
-          }
-        })
-      })
-    })
   }
 }
 
