a7899873b1b7644c9b4ce04b3d5d8cdfab4cab81
Muhammad Irfan <m.irfan@bukalapak.com>
2018-04-05 23:07:09 +0700

    initial commit

---
+++ commands/liquid_asset.js
@@ -0,0 +1,31 @@
+const sqlite3 = require('sqlite3').verbose()
+const db = new sqlite3.Database('db/database')
+const helper = require('./_command_helper.js')
+
+module.exports = {
+  process: function(chat_id, input, bot) {
+    // Setup variables
+    const query = 'SELECT * FROM accounts'
+    db.all(query, (err, rows) => {
+      if (err) return bot.sendMessage(chat_id, "Error: " + err)
+
+      // Build message
+      let msgArr = []
+      rows.forEach(row => {
+        let msg = `
+          Name: ${row.name}
+          Amount: ${row.amount}
+
+        `
+        msgArr.push(msg)
+      })
+
+      let message = msgArr.join("\n")
+
+      helper.deleteContext(chat_id, () => {
+        bot.sendMessage(chat_id, message)
+      })
+    })
+
+  }
+}
