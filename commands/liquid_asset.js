c1df6d86248502cc28db594a9b00d4c2cadee5d4
Muhammad Irfan <naprirfan@gmail.com>
2018-04-05 16:11:49 +0000

    fix query and format

--- commands/liquid_asset.js
+++ commands/liquid_asset.js
@@ -5,17 +5,15 @@ const helper = require('./_command_helper.js')
 module.exports = {
   process: function(chat_id, input, bot) {
     // Setup variables
-    const query = 'SELECT * FROM accounts'
+    const query = 'SELECT * FROM account'
     db.all(query, (err, rows) => {
       if (err) return bot.sendMessage(chat_id, "Error: " + err)
 
       // Build message
       let msgArr = []
       rows.forEach(row => {
-        let msg = `
-          Name: ${row.name}
-          Amount: ${row.amount}
-
+        let msg = `Name: ${row.name}
+Amount: ${row.amount}
         `
         msgArr.push(msg)
       })
